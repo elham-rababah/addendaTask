7d636c2a8d4fbe4fe78a6cba104cc5abd39bdc3c
Elham Rababah <elham@model.com>
2020-01-25 22:54:22 +0200

    addenda: file structure  and html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { TweetsComponent } from './tweets/tweets.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component:LoginComponent},{path:'tweets',component:TweetsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TweetsComponent } from './tweets/tweets.component';
+import { TweetComponent } from './tweets/tweet/tweet.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { TweetDetailsComponent } from './tweets/tweet/tweet-details/tweet-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TweetsComponent,
+    TweetComponent,
+    HeaderComponent,
+    FooterComponent,
+    TweetDetailsComponent
   ],
   imports: [
     BrowserModule,
